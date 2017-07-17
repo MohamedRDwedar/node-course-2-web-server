69c2d5e061d3ddb13708f964289deb6ae14a938e
Mohamed Dwedar <mrefat.dwedar@gmail.com>
2017-07-17 18:30:03 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,69 @@
+ const express = require('express');
+ const hbs = require('hbs');
+ const fs = require('fs');
+
+ var app = express();
+
+ hbs.registerPartials(__dirname + '/views/partials');
+ app.set('view engine', 'hbs');
+
+ app.use((req, res, next) => {
+     var now = new Date().toString();
+     var log = `${now}: ${req.method}, ${req.url}`;
+     console.log(log);
+     fs.appendFile('server.log.txt', log + '\n', (error) => {
+         if (error) {
+             console.log('Unable to append to server.log file');
+         }
+     });
+     next();
+ });
+
+ //  app.use((req, res, next) => {
+ //      res.render('maintenance.hbs', {
+ //          pageTitle: 'Maintenance Page',
+ //          welcomeMessage: 'We are now in maintenance mode we will back soon.'
+ //      });
+ //  });
+
+ hbs.registerHelper('getCurrentYear', () => {
+     return new Date().getFullYear();
+ });
+
+ app.use(express.static(__dirname + '/public'));
+
+ hbs.registerHelper('screamIt', (text) => {
+     return text.toUpperCase();
+ });
+
+ app.get('/', (req, res) => {
+     res.send({
+         name: 'Mohamed',
+         likes: [
+             'Novels',
+             'Gamed',
+             'Latptops'
+         ]
+     })
+ });
+
+ app.get('/about', (req, res) => {
+     res.render('about.hbs', {
+         pageTitle: 'About Page'
+     });
+ });
+
+ app.get('/home', (req, res) => {
+     res.render('home.hbs', {
+         pageTitle: 'Home Page',
+         welcomeMessage: 'Welecome to home page'
+     });
+ });
+
+ app.get('/bad', (req, res) => {
+     res.send({
+         errorMessage: 'unable to handle requset.'
+     });
+ });
+
+ app.listen(3000);
